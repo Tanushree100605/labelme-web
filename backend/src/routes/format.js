64228f0e25db8eab1f0c14368bb879e2111b9828
Vikrant2004-cmd <vikrantbhadauria07@gmail.com>
2026-07-31 04:16:45 +0530

    remove old backend

--- backend/src/routes/format.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getFolderFormat, resetFolderFormat } = require("../controllers/formatController");
-
-router.get("/", getFolderFormat);
-router.post("/reset", resetFolderFormat);
-
-module.exports = router;
