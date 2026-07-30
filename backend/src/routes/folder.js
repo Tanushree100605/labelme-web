d23d45653c402bc9c9b1be4f1b144220b457d976
Vikrant2004-cmd <vikrantbhadauria07@gmail.com>
2026-07-31 04:16:26 +0530

    remove old backend

--- backend/src/routes/folder.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { listFolder, serveImage } = require("../controllers/folderController");
-
-router.get("/list", listFolder);
-router.get("/image", serveImage);
-
-module.exports = router;
