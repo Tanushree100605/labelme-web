6127f6a50e10225af06d54907b2b39763d3c10e9
Vikrant2004-cmd <vikrantbhadauria07@gmail.com>
2026-07-31 04:16:08 +0530

    remove old backend

--- backend/src/routes/annotations.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { saveAnnotation, loadAnnotation } = require("../controllers/annotationController");
-
-router.post("/save", saveAnnotation);
-router.get("/load", loadAnnotation);
-
-module.exports = router;
